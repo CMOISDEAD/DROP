e40b5f3b1c92244766e5ec9e2b05137c89ca26a7
CMOISDEAD <camilodavina@gmail.com>
2021-01-22 12:05:48 -0500

    agregando funcionalidad

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 const env = require('node-env-file')
-const { Kayn } = require('kayn')
+const { Kayn, REGIONS } = require('kayn')
 const myConfig = require('./configs/kaynConfig')
-
+const fs = require('fs')
 // Initialize
 env(__dirname + '/.env')
 const kayn = Kayn(process.env.KEY)(myConfig)
 
-requestSummoner = (name) =>{
-    kayn.Summoner.by.name(name)
-    .then(summoner => {
-        console.log(summoner)
+
+kayn.DDragon.Champion.list()
+    .version('11.1.1')
+    .then(res => {
+        const champions = res.data
+        const listaCampeones = []
+        for (const champs in champions) {
+            if (champions.hasOwnProperty.call(champions, champs)) {
+                listaCampeones.push(champs)
+            }
+        }
     })
-    .catch(error => console.error(error))
-}
-
-requestSummoner('yassuo')
-
-
-
-
+    .catch(err => console.error(err))
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,33 +9,51 @@ const env = require('node-env-file')
 env(path.join(__dirname, '../.env'))
 const kayn = Kayn(process.env.KEY)(myConfig)
 
+// Global variables
+const listaCampeones = []
+
 requestSummoner = (data) => {
     return kayn.Summoner.by.name(data)
         .then(res => res)
-        .catch(error => console.error(error))
+        .catch(error => {
+            return 'null'
+            console.error(error)
+        })
 }
 
-requestChamps = (data) => {
-    return kayn.DDragon.Champion.list(data)
-        .then(res => {
-            var champions = res.data
-            return champions
-        })
-        .catch(err => console.error(err))
+requestChamps = () => {
+    kayn.DDragon.Champion.list()
+    .version('11.1.1')
+    .then(res => {
+        const champions = res.data
+        for (const champs in champions) {
+            if (champions.hasOwnProperty.call(champions, champs)) {
+                listaCampeones.push(champs)
+            }
+        }
+    })
+    .catch(err => console.error(err))
 }
+
+requestChamps()
 
 router.get("/", (req, res) => {
     res.render("pages/home");
 });
 
-router.get('/champs', async (req, res) => {
-    var champions = await requestChamps('11.1.1')
+router.get('/champs', async (req, res) => {    
+    const alterna = {
+        name : listaCampeones
+    }
+    res.render('pages/champs', alterna)
+})
 
-    // champions.forEach(name => {
-    //     console.log(name.id)
-    // });
-
-    res.render('pages/champs', champions)
+router.get('/champs/:name', (req, res)=>{
+    const { name } = req.params
+    const championToSend = {
+        name
+    }
+    res.render('pages/champInfo', championToSend)
 })
 
 router.get('/summoner', (req, res) => {
@@ -44,6 +62,10 @@ router.get('/summoner', (req, res) => {
 
 router.get('/about', (req, res) => {
     res.send('about')
+})
+
+router.get('/dev', (req, res) => {
+    res.send('dev')
 })
 
 router.get('/summoner/search', async (req, res) => {
